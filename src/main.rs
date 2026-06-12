1ab84c0a5cb966196afb8334a541c005dc34b43f
Stjepan Poljak <stjepan.poljak@protonmail.com>
2026-06-12 20:04:15 +0200

    Make vcpu and vm into proper structs.

--- src/main.rs
+++ src/main.rs
@@ -22,10 +22,6 @@ const KVM_GET_REGS : u64 = _IOR::<kvm_regs>(KVMIO, 0x81);
 const KVM_SET_REGS : u64 = _IOW::<kvm_regs>(KVMIO, 0x82);
 const KVM_RUN : u64 = _IO(KVMIO, 0x80);
 
-pub struct KvmDev {
-    pub file: File
-}
-
 #[derive(Parser, Debug)]
 #[command(version, about)]
 struct Args {
@@ -45,6 +41,10 @@ fn load_binary(path: &str) -> io::Result<Vec<u8>> {
     Ok(res)
 }
 
+pub struct KvmDev {
+    pub file: File
+}
+
 impl KvmDev {
     fn new() -> Result<Self, Box<dyn std::error::Error>> {
 	let file = OpenOptions::new()
@@ -55,9 +55,23 @@ impl KvmDev {
 	Ok(Self { file })
     }
 
+    fn create_vm(&self) -> Result<VM, Box<dyn std::error::Error>> {
+
+	let vm_fd = unsafe { libc::ioctl(self.fd(), KVM_CREATE_VM, 0usize) };
+	if vm_fd < 0 {
+            return Err(Box::new(io::Error::last_os_error()));
+	}
+
+	Ok(VM { fd: vm_fd })
+    }
+
     fn fd(&self) -> libc::c_int {
 	self.file.as_raw_fd()
     }
+}
+
+pub struct VM {
+    pub fd: libc::c_int
 }
 
 pub struct MemRegion {
@@ -66,8 +80,24 @@ pub struct MemRegion {
     pub guest_phys_addr: u64
 }
 
-impl MemRegion {
-    fn new(vm_fd: libc::c_int, mem_size: usize, guest_phys_addr: u64) -> Result<Self, Box<dyn std::error::Error>> {
+impl Drop for MemRegion {
+    fn drop(&mut self) {
+        unsafe { libc::munmap(self.mem_ptr, self.mem_size); }
+    }
+}
+
+impl VM {
+    fn create_vcpu(&self) -> Result<VCPU, Box<dyn std::error::Error>> {
+	// 140904 ioctl(9<anon_inode:kvm-vm>, 0xae41 /* KVM_CREATE_VCPU */, 0) = 10<anon_inode:kvm-vcpu:0>
+	let vcpu_fd = unsafe { libc::ioctl(self.fd, KVM_CREATE_VCPU, 0usize) };
+	if vcpu_fd < 0 {
+            return Err(Box::new(io::Error::last_os_error()));
+	}
+
+	Ok(VCPU { fd: vcpu_fd })
+    }
+
+    fn create_mem_region(&self, mem_size: usize, guest_phys_addr: u64) -> Result<MemRegion, Box<dyn std::error::Error>> {
 	// 140900 mmap(NULL, 1075838976, 0 /* PROT_NONE */, 0x22 /* MAP_PRIVATE|MAP_ANONYMOUS */, -1, 0) = 0x7768b3e00000
 	// 140900 mmap(0x7768b3e00000, 1073741824, 0x3 /* PROT_READ|PROT_WRITE */, 0x32 /* MAP_PRIVATE|MAP_FIXED|MAP_ANONYMOUS */, -1, 0) = 0x7768b3e00000
 	let mem_ptr = unsafe {
@@ -91,52 +121,49 @@ impl MemRegion {
 	    userspace_addr : mem_ptr as u64
 	};
 
-	let ret = unsafe { libc::ioctl(vm_fd, KVM_SET_USER_MEMORY_REGION, &region) };
+	let ret = unsafe { libc::ioctl(self.fd, KVM_SET_USER_MEMORY_REGION, &region) };
 	if ret < 0 {
             return Err(Box::new(io::Error::last_os_error()));
 	}
 
-	Ok(Self { mem_ptr, mem_size, guest_phys_addr })
+	Ok(MemRegion { mem_ptr, mem_size, guest_phys_addr })
     }
 }
 
-impl Drop for MemRegion {
+impl Drop for VM {
     fn drop(&mut self) {
-        unsafe { libc::munmap(self.mem_ptr, self.mem_size); }
+	unsafe { libc::close(self.fd); }
     }
 }
 
-fn create_vm(kvm_dev: &KvmDev) -> Result<libc::c_int, Box<dyn std::error::Error>> {
+pub struct VCPU {
+    pub fd: libc::c_int
+}
 
-    let vm_fd = unsafe { libc::ioctl(kvm_dev.fd(), KVM_CREATE_VM, 0usize) };
-    if vm_fd < 0 {
-        return Err(Box::new(io::Error::last_os_error()));
+impl Drop for VCPU {
+    fn drop(&mut self) {
+	unsafe { libc::close(self.fd); }
     }
-
-    Ok(vm_fd)
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
     let kvm_dev = KvmDev::new()?;
-    let vm_fd = create_vm(&kvm_dev)?;
-
-    let mem_region = MemRegion::new(vm_fd, args.memory * 1024, 0x0)?;
-
-    // 140904 ioctl(9<anon_inode:kvm-vm>, 0xae41 /* KVM_CREATE_VCPU */, 0) = 10<anon_inode:kvm-vcpu:0>
-    let vcpu_fd = unsafe { libc::ioctl(vm_fd, KVM_CREATE_VCPU, 0usize) };
+    let vm = kvm_dev.create_vm()?;
+    let mem_region = vm.create_mem_region(args.memory * 1024, 0x0)?;
+    let vcpu = vm.create_vcpu()?;
 
     // 140904 ioctl(3</dev/kvm<char 10:232>>, 0xae04 /* KVM_GET_VCPU_MMAP_SIZE */, 0) = 12288
     // 140904 mmap(NULL, 12288, 0x3 /* PROT_READ|PROT_WRITE */, 0x1 /* MAP_SHARED */, 10<anon_inode:kvm-vcpu:0>, 0) = 0x7769050b0000
     let kvm_run_size = unsafe { libc::ioctl(kvm_dev.fd(), KVM_GET_VCPU_MMAP_SIZE, 0usize) };
-    let kvm_run_mem = unsafe { libc::mmap(ptr::null_mut(), kvm_run_size as usize, libc::PROT_READ|libc::PROT_WRITE, libc::MAP_SHARED, vcpu_fd, 0) };
+    let kvm_run_mem = unsafe { libc::mmap(ptr::null_mut(), kvm_run_size as usize, libc::PROT_READ|libc::PROT_WRITE, libc::MAP_SHARED, vcpu.fd, 0) };
 
     // 140904 ioctl(10<anon_inode:kvm-vcpu:0>, 0x8140aecc /* KVM_GET_SREGS2 */, 0x77690198f310) = 0
     let mut sregs : kvm_sregs2;
     unsafe {
 	sregs = std::mem::zeroed();
-	libc::ioctl(vcpu_fd, KVM_GET_SREGS2, &mut sregs);
+	libc::ioctl(vcpu.fd, KVM_GET_SREGS2, &mut sregs);
     }
 
     sregs.cs.base = 0;
@@ -145,14 +172,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("CS\tbase={:#x}\n\tselector={:#x}\nCR0={:#x}", sregs.cs.base, sregs.cs.selector, sregs.cr0);
 
     unsafe {
-	libc::ioctl(vcpu_fd, KVM_SET_SREGS2, &mut sregs);
+	libc::ioctl(vcpu.fd, KVM_SET_SREGS2, &mut sregs);
     }
 
     // 140904 ioctl(10<anon_inode:kvm-vcpu:0>, 0x8090ae81 /* KVM_GET_REGS */, {rax=0, ..., rsp=0, rbp=0, ..., rip=0xfff0, rflags=0x2}) = 0
     let mut regs : kvm_regs;
     unsafe {
 	regs = std::mem::zeroed();
-	libc::ioctl(vcpu_fd, KVM_GET_REGS, &mut regs);
+	libc::ioctl(vcpu.fd, KVM_GET_REGS, &mut regs);
     }
 
     regs.rip = 0x1000;
@@ -160,7 +187,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("RIP={:#x} RSP={:#x} RFLAGS={:#x}", regs.rip, regs.rsp, regs.rflags);
 
     unsafe {
-	libc::ioctl(vcpu_fd, KVM_SET_REGS, &mut regs)
+	libc::ioctl(vcpu.fd, KVM_SET_REGS, &mut regs)
     };
 
     let code = load_binary(args.binary.as_str())?;
@@ -174,7 +201,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
 
     unsafe {
-	libc::ioctl(vcpu_fd, KVM_RUN, 0usize)
+	libc::ioctl(vcpu.fd, KVM_RUN, 0usize)
     };
 
     let exit_reason = unsafe { (*(kvm_run_mem as *mut kvm_run)).exit_reason };
